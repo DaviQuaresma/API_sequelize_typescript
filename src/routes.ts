0a3611e7e96101ee420c973b2fae6f47681df61e
DaviGuruKiller <quaresma.davi@gmail.com>
2025-03-24 12:12:22 -0300

    Revert "ok"
    This reverts commit 763e91d7882137e31182ad662f97e5af30942cd1.

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,6 @@
 
 import express from "express";
 import { candidatesController } from "./controllers/candidates-controllers";
-import { companiesController } from "./controllers/companies-controllers";
 
 const router = express.Router();
 
@@ -13,11 +12,5 @@ router.get("/candidates/:id", candidatesController.show);
 router.post("/candidates", candidatesController.save);
 router.put("/candidates/:id", candidatesController.update);
 router.delete("/candidates/:id", candidatesController.delete);
-
-router.get("/companies", companiesController.index);
-router.post("/companies", companiesController.save);
-router.get("/companies/:id", companiesController.show);
-router.put("/companies/:id", companiesController.update);
-router.delete("/companies/:id", companiesController.delete);
 
 export { router };
