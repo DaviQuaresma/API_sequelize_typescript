29c4e9e40e01b5177e0f7a0331d8cdbb121a4a42
DaviGuruKiller <quaresma.davi@gmail.com>
2025-03-23 21:13:51 -0300

    ok

---
+++ src/database/index.ts
@@ -0,0 +1,13 @@
+/** @format */
+
+import { Sequelize } from "sequelize";
+
+const dbUrl = process.env.DATABASE_URL || "";
+
+const sequelize = new Sequelize(dbUrl, {
+	define: {
+		underscored: true,
+	},
+});
+
+export { sequelize };
