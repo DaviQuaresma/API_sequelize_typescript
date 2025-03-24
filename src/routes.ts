763e91d7882137e31182ad662f97e5af30942cd1
DaviGuruKiller <quaresma.davi@gmail.com>
2025-03-24 01:00:28 -0300

    ok

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@
 
 import express from "express";
 import { candidatesController } from "./controllers/candidates-controllers";
+import { companiesController } from "./controllers/companies-controllers";
 
 const router = express.Router();
 
@@ -12,5 +13,11 @@ router.get("/candidates/:id", candidatesController.show);
 router.post("/candidates", candidatesController.save);
 router.put("/candidates/:id", candidatesController.update);
 router.delete("/candidates/:id", candidatesController.delete);
+
+router.get("/companies", companiesController.index);
+router.post("/companies", companiesController.save);
+router.get("/companies/:id", companiesController.show);
+router.put("/companies/:id", companiesController.update);
+router.delete("/companies/:id", companiesController.delete);
 
 export { router };
